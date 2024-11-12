91326e7e0decf5a941c584fa740e94143dcfec82
Babatenn <131091935+Babatenn@users.noreply.github.com>
2024-11-12 23:06:50 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,5 +1,3 @@
 function sayhello68(){alert('Hello,'+document.getElementById('who68').innerHTML+'!');}
 function taro68(){alert('Hello,'+document.getElementById('who68').innerHTML='太郎';}
 function hanako68(){alert('Hello,'+document.getElementById('who68').innerHTML='花子';}
-
-function gazo688(){document.getElementById('who688').innerHTML='<img scr="neko.jpg"alt="オン">';}
