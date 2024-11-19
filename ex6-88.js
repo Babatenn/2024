82caea9ad29818d2a5887a7126e0ae29f8b4f5d4
Babatenn <131091935+Babatenn@users.noreply.github.com>
2024-11-19 09:53:50 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function gazo688(){document.getElementById('who688').innerHTML='<img scr="./image/elf.png"alt="オン">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img scr="./image/elf.png" alt="オン">';}
