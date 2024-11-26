c08ebe75c330795fecfe84efa62718edf5eb2555
Babatenn <131091935+Babatenn@users.noreply.github.com>
2024-11-26 21:46:39 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,2 @@
-function imgchange3(){document.getElementById('logo').src'./image2/chara.jpg';}
-function imgchange4(){document.getElementById('logo').src'./image2/josailogo.jpg';}
+function imgchange3(){document.getElementById('logo').src'./image/sai.jpg';}
+function imgchange4(){document.getElementById('logo').src'./image/Josaiogo.jpg';}
