e9f5a5aed2b9f8fcca0a28626edad60a7ef22c7f
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-07 09:32:00 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,4 +1,4 @@
 function add73(){
- var a=document.getEiementById('input731').value;
- var b=document.getEiementById('input732').value;
+ var a=document.getElementById('input731').value;
+ var b=document.getElementById('input732').value;
  alert(Number(a)/(Number(b)*Number(b)));}
